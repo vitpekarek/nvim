30d53e539573b6953293540a3618c5ee6297561f
Vit Pekarek <vit.pekarek@outlook.com>
2021-04-06 18:12:22 +0200

    updated telescope configuration

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,52 +1,22 @@
-local actions = require('telescope.actions')
-
 require'telescope'.setup {
-    defaults = {
-        timeoutlen = 2000,
-        mappings = {i = {["<esc>"] = actions.close, }},
-        vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            -- '--with-filename',
-            -- '--line-number',
-            -- '--column',
-            -- '--smart-case'
-        },
-        prompt_position = "top",
-        prompt_prefix = ">",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_defaults = {
-            -- TODO add builtin options.
-        },
-        file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-        file_ignore_patterns = {".backup",".swap",".langservers",".session",".undo","*.git","node_modules","vendor",".cache",".vscode-server",".Desktop",".Documents","classes"},
-        generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-        shorten_path = true,
-        winblend = 0,
-        width = 0.75,
-        preview_cutoff = 120,
-        results_height = 1,
-        results_width = 0.8,
-        border = {},
-        borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-        color_devicons = true,
-        use_less = true,
-        set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-        file_previewer = require'telescope.previewers'.cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
-        grep_previewer = require'telescope.previewers'.vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
-        qflist_previewer = require'telescope.previewers'.qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
-
-        -- Developer configurations: Not meant for general override
-        -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+  defaults = {
+    layout_strategy = 'flex',
+    scroll_strategy = 'cycle',
+    winblend = 0,
+    layout_defaults = {
+      horizontal = {
+        width_padding = 0.1,
+        height_padding = 0.1,
+        preview_width = 0.6,
+        -- mirror = false,
+      },
+      vertical = {
+        width_padding = 0.05,
+        height_padding = 1,
+        preview_height = 0.5,
+        -- mirror = true,
+      }
     },
-    extensions = {
-        fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        },
-    },
+    file_ignore_patterns = { 'tags' },
+  }
 }
