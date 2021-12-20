d56e12493dcf3809daae319ed0b42ace6aeadb0a
Vit Pekarek <vit.pekarek@outlook.com>
2021-12-20 12:39:04 +0100

    dashboard update

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -4,10 +4,10 @@ vim.g.dashboard_custom_section = {
     a = {description = {'  Find File          '}, command = 'Telescope find_files'},
     b = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
     c = {description = {'  Projects           '}, command = 'Telescope projects'},
-    d = {description = {'  Files              '}, command = 'Telescope projects'},
-    -- c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
-    e = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    f = {description = {'  Settings           '}, command = ':e ~/AppData/Local/nvim/lua/settings.lua'},
+    d = {description = {'  File Browser       '}, command = 'Telescope file_browser'},
+    e = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
+    f = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
+    g = {description = {'  Settings           '}, command = ':e ~/AppData/Local/nvim/lua/settings.lua'},
     -- f = {description = {'  Marks              '}, command = 'Telescope marks'}
 }
 
